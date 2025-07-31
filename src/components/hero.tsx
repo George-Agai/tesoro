63e9efa9d3301dbee97beb2b84f5bc2af987273a
George Agai <georgeagai2000@gmail.com>
2025-07-31 21:31:52 +0300

    Set up tailwind css

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,13 @@
 export const Hero: React.FC = () => (
-    <section className="container text-center py-12">
-      <h1 className="text-3xl md:text-5xl font-bold mb-4">
-        Send money instantly
-      </h1>
-      <p className="text-lg md:text-xl mb-8">
-        Fast, secure, and low-cost payments, wherever you are.
-      </p>
-      <button className="px-6 py-3 rounded bg-var(--color-primary) text-blue-500">
-        Get Started
-      </button>
+    <section className="my-container text-center py-8 px-1 bg-purple-200">
+        <h1 className="text-3xl md:text-5xl font-bold mb-1 mt-1">
+            Send money instantly
+        </h1>
+        <p className="text-lg md:text-xl mb-1">
+            Fast, secure, and low-cost payments, wherever you are.
+        </p>
+        <button className="px-2 py-0.5 mt-1 mb-1 border border-green-500 text-green-500 rounded-lg hover:bg-green-500 hover:text-white transition">
+            Get Started
+        </button>
     </section>
-  );
-  
+);
